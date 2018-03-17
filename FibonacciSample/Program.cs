e2b320cda72e4f131e502e16ca9fc1ee4564c310
gushwell <gushwell@gmail.com>
2018-03-17 20:28:14 +0900

    フィボナッチ数を列挙する

--- FibonacciSample/Program.cs
+++ FibonacciSample/Program.cs
@@ -1,9 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FibonacciSample {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            // f(0)..f(50)までを列挙する
+            var fibos = Fibonacci.Enumerate()
+                                 .Select((Value, Index) => new { Index, Value })
+                                 .TakeWhile(x => x.Index <= 50);
+            foreach (var f in fibos) {
+                Console.WriteLine($"f({f.Index}) = {f.Value}");
+            }
+        }
+    }
+
+    static class Fibonacci {
+        public static IEnumerable<long> Enumerate() {
+            yield return 0;
+            yield return 1;
+            // 無限に求める オーバーフローは無視
+            long[] array = new long[] { 0, 1 };
+            while (true) {
+                var fibo = array[0] + array[1];
+                array[0] = array[1];
+                array[1] = fibo;
+                yield return fibo;
+            }
         }
     }
 }
