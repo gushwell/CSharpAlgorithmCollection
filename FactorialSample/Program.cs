fdb7565792918c34f0b637e99ccc8991ba3086ee
gushwell <gushwell@gmail.com>
2018-03-17 19:44:15 +0900

    再帰処理で、nの階乗を計算する

--- FactorialSample/Program.cs
+++ FactorialSample/Program.cs
@@ -3,7 +3,17 @@
 namespace FactorialSample {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            for (var n = 0; n < 15; n++)
+                Console.WriteLine(Factorial(n));
+        }
+
+        // 再帰処理で、nの階乗を計算する　　
+        public static long Factorial(int n) {
+            checked {
+                if (n == 0)
+                    return 1L;
+                return n * Factorial(n - 1);
+            }
         }
     }
 }
