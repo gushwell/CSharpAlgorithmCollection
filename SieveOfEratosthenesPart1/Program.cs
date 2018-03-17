a8353df908830555dc6bdb66dd629ad95be33ce2
gushwell <gushwell@gmail.com>
2018-03-17 20:06:39 +0900

    エラトステネスの篩で素数を列挙する。 通常バージョン

---
+++ SieveOfEratosthenesPart1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SieveOfEratosthenesPart1 {
+    class Program {
+        static void Main(string[] args) {
+            foreach (var n in Primes(100000))
+                Console.Write("{0,3} ", n);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        static IEnumerable<int> Primes(int maxnum) {
+            bool[] sieve = Enumerable.Repeat(true, maxnum + 1).ToArray();
+            int squareroot = (int)Math.Sqrt(maxnum);
+            for (int i = 2; i <= squareroot; i++) {
+                if (sieve[i] == false)
+                    continue;
+                for (int n = i * 2; n <= maxnum; n += i)
+                    sieve[n] = false;
+            }
+            for (int i = 2; i <= maxnum; i++)
+                if (sieve[i] == true)
+                    yield return i;
+        }
+    }
+}
---
+++ SieveOfEratosthenesPart2/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SieveOfEratosthenesPart2 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
