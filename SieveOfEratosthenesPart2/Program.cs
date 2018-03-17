beb78c7d53fda34e197ed8476c5271f2833c2eb8
gushwell <gushwell@gmail.com>
2018-03-17 20:08:00 +0900

    エラトステネスの篩で素数を列挙する。 列挙開始を早くするバージョン

--- SieveOfEratosthenesPart2/Program.cs
+++ SieveOfEratosthenesPart2/Program.cs
@@ -1,9 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SieveOfEratosthenesPart2 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            foreach (var n in Primes(100000))
+                Console.Write("{0,3} ", n);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        static IEnumerable<int> Primes(int maxnum) {
+            yield return 2;
+            yield return 3;
+            var sieve = new System.Collections.BitArray(maxnum + 1);
+            int squareroot = (int)Math.Sqrt(maxnum);
+            for (int i = 2; i <= squareroot; i++) {
+                if (sieve[i] == false) {
+                    for (int n = i * 2; n <= maxnum; n += i)
+                        sieve[n] = true;
+                }
+                for (int n = i * i + 1; n <= maxnum && n < (i + 1) * (i + 1); n++) {
+                    if (!sieve[n])
+                        yield return n;
+                }
+            }
         }
     }
 }
