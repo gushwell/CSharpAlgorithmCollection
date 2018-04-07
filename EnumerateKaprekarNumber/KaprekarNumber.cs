25dec1f9e8bbc611812620e47ed3cd0d017d292b
gushwell <gushwell@gmail.com>
2018-04-07 17:57:51 +0900

    カプレカ数を列挙する

---
+++ EnumerateKaprekarNumber/KaprekarNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnumerateKaprekarNumber {
+    public class KaprekarValue {
+        public long Number { get; set; }
+        public int Part1 { get; set; }
+        public int Part2 { get; set; }
+    }
+
+    public class KaprekarNumber {
+
+        public static IEnumerable<int> Enumerate() {
+            for (int n = 1; n <= int.MaxValue; n++) {
+                var kv = Analyze(n);
+                if (kv != null)
+                    yield return n;
+            }
+        }
+
+        public static KaprekarValue Analyze(int x) {
+            long x2 = (long)x * x;
+            var xDigits = (int)Math.Log10(x) + 1;
+            var x2Digits = (int)Math.Log10(x2) + 1;
+            for (int n = x2Digits - xDigits; n <= x2Digits; n++) {
+                var dn = (long)Math.Pow(10, n);
+                var a = x2 / dn;
+                var b = x2 % dn;
+                if ((0 < b && b < dn) && (a + b == x)) {
+                    return new KaprekarValue {
+                        Number = x,
+                        Part1 = (int)a,
+                        Part2 = (int)b
+                    };
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ EnumerateKaprekarNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace EnumerateKaprekarNumber {
+    class Program {
+        static void Main(string[] args) {
+            var kaprekars = KaprekarNumber.Enumerate();
+            foreach (var n in kaprekars.Take(50)) {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
