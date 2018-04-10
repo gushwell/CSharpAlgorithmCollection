79837286438e9fc3ffdb8162ff421ae9c1c4db91
gushwell <gushwell@gmail.com>
2018-04-10 20:36:48 +0900

    ミラー・ラビン素数判定法による素数判定メソッド

---
+++ PrimeByMillarRrabin/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PrimeByMillarRrabin {
+    
+    class Program {
+        static void Main(string[] args) {
+            for (var i = 1990000; i < 2010000; i++) {
+                var isPrime = PrimeNumber.IsPrime(i);
+                if (isPrime)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
