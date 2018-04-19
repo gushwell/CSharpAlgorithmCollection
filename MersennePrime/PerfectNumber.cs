b22ec300645aa5fef59ae9c933afff457f03b8d5
gushwell <gushwell@gmail.com>
2018-04-19 20:33:04 +0900

    メルセンヌ素数を利用して完全数を求める

---
+++ MersennePrime/PerfectNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MersennePrime {
+    public class PerfectNumber {
+        public static IEnumerable<long> Take(int maxcount) {
+            int count = 0;
+            long n = 2;
+            while (count < maxcount) {
+                long mp = (long)Math.Pow(2, n) - 1;
+                if (PrimeNumber.IsPrime(mp)) {
+                    long pn = (long)(Math.Pow(2, n - 1) * mp);
+                    count++;
+                    yield return pn;
+                }
+                n++;
+            }
+        }
+    }
+}
---
+++ MersennePrime/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MersennePrime {
+    class Program {
+        static void Main(string[] args) {
+            var q = PerfectNumber.Take(8);
+            foreach (var n in q) {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
