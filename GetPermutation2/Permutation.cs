87410876cf42ede43344031c17465d874414c620
gushwell <gushwell@gmail.com>
2018-06-26 20:59:11 +0900

    n個の要素の中から k個を選ぶ順列

---
+++ GetPermutation2/Permutation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GetPermutation2 {
+    public class Permutation {
+        public IEnumerable<T[]> Enumerate<T>(IEnumerable<T> items, int k, bool withRepetition) {
+            if (k == 1) {
+                foreach (var item in items) {
+                    yield return new T[] { item };
+                }
+                yield break;
+            }
+            foreach (var item in items) {
+                var leftside = new T[] { item };
+                var unused = withRepetition ? items : items.Except(leftside);
+                foreach (var rightside in Enumerate(unused, k - 1, withRepetition)) {
+                    yield return leftside.Concat(rightside).ToArray();
+                }
+            }
+        }
+
+        internal IEnumerable<object[]> Enumerate(string v1, int v2) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GetPermutation2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GetPermutation2 {
+    class Program {
+        static void Main(string[] args) {
+            var perm = new Permutation();
+
+            // 重複なし
+            foreach (var n in perm.Enumerate("ABCD", 3, false)) {
+                foreach (var x in n)
+                    Console.Write("{0} ", x);
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            // 重複あり
+            foreach (var n in perm.Enumerate("ABCD", 3, true)) {
+                foreach (var x in n)
+                    Console.Write("{0} ", x);
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
