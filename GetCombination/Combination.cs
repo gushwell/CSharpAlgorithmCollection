7691eb59392c2ee1c203e28680a3f9e9c24a2e38
gushwell <gushwell@gmail.com>
2018-06-29 20:15:43 +0900

    n個の要素からk個を選ぶ組合せを列挙する

---
+++ GetCombination/Combination.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GetCombination {
+    public static class Combination {
+        public static IEnumerable<T[]> Enumerate<T>(IEnumerable<T> items, int k, bool withRepetition) {
+            if (k == 1) {
+                foreach (var item in items)
+                    yield return new T[] { item };
+                yield break;
+            }
+            foreach (var item in items) {
+                var leftside = new T[] { item };
+
+                // item よりも前のものを除く （順列と組み合わせの違い)
+                // 重複を許さないので、unusedから item そのものも取り除く
+                var unused = withRepetition 
+                            ? items 
+                            : items.SkipWhile(e => !e.Equals(item)).Skip(1).ToList();
+
+                foreach (var rightside in Enumerate(unused, k - 1, withRepetition)) {
+                    yield return leftside.Concat(rightside).ToArray();
+                }
+            }
+        }
+    }
+}
---
+++ GetCombination/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace GetCombination {
+    class Program {
+        static void Main(string[] args) {
+            int n = 5;
+            int k = 3;
+            var nums = Enumerable.Range(1, n).ToArray();
+            var combinations = Combination.Enumerate(nums, k, withRepetition: false);
+
+            int i = 1;
+            foreach (var elem in combinations) {
+                string s = "(" + string.Join(",", elem.Select(x => x.ToString()).ToArray()) + ")";
+                Console.WriteLine("{0} : {1}", i++, s);
+            }
+        }
+    }
+}
