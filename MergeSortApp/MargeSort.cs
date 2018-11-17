1796e37d689de3bc892f706140e8d5d1f3622ef0
gushwell <gushwell@gmail.com>
2018-11-17 13:27:34 +0900

    マージソート

---
+++ MergeSortApp/MargeSort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MergeSortApp {
+    static class MargeSort {
+        public static IEnumerable<T> Sort<T>(IEnumerable<T> items, Comparison<T> compare) {
+            if (items.Count() > 1) {
+                int m = items.Count() / 2;
+                var a1 = items.Take(m).ToArray();
+                var a2 = items.Skip(m).ToArray();
+                return Merge(Sort(a1, compare), Sort(a2, compare), compare);
+            }
+            return items;
+        }
+
+        // 非再帰版　Mergeメソッド
+        private static IEnumerable<T> Merge<T>(IEnumerable<T> a1, IEnumerable<T> a2, Comparison<T> compare) {
+            var ite1 = a1.GetEnumerator();
+            var ite2 = a2.GetEnumerator();
+            var exists1 = ite1.MoveNext();
+            var exists2 = ite2.MoveNext();
+            while (exists1 == true && exists2 == true) {
+                T x1 = ite1.Current;
+                T x2 = ite2.Current;
+                if (compare(x1, x2) < 0) {
+                    yield return x1;
+                    exists1 = ite1.MoveNext();
+                } else {
+                    yield return x2;
+                    exists2 = ite2.MoveNext();
+                }
+            }
+            while (exists1) {
+                yield return ite1.Current;
+                exists1 = ite1.MoveNext();
+            }
+            while (exists2) {
+                yield return ite2.Current;
+                exists2 = ite2.MoveNext();
+            }
+        }
+    }
+}
---
+++ MergeSortApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MergeSortApp {
+    // MergeSortの検証用コード
+    class Program {
+        static void Main(string[] args) {
+            Random rnd = new Random();
+            var nums = new int[10000];
+            for (int count = 0; count < 10; count++) {
+                for (int i = 0; i < nums.Length; i++) {
+                    nums[i] = rnd.Next(1, 10000);
+                }
+
+                var result = MargeSort.Sort(nums, (a, b) => a - b);
+                // LINQのOrderByメソッドの結果と比較することで、MergeSortが正しく整列されているかを確認している
+                bool isEqual = Enumerable.SequenceEqual(result, nums.OrderBy(n => n));
+                Console.WriteLine(isEqual);
+            }
+            Console.WriteLine("end");
+        }
+    }
+}
