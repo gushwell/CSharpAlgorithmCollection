46a730b5f26ea5c2963d8d0c39b0dd3792f6add8
gushwell <gushwell@gmail.com>
2018-11-27 17:43:08 +0900

    簡易catコマンド -　IEnumerable<T>の実装サンプル

---
+++ CatCommand/EnumerableTextReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CatCommand {
+    public class EnumerableTextReader : IEnumerable<string> {
+        private TextReader _sr;
+
+        public EnumerableTextReader(TextReader tr) => _sr = tr;
+
+        public EnumerableTextReader(Stream s) => _sr = new StreamReader(s);
+
+        public IEnumerator<string> GetEnumerator() {
+            string s;
+            while ((s = _sr.ReadLine()) != null) {
+                yield return s;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+    }
+}
---
+++ CatCommand/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatCommand {
+    class Program {
+        static void Main() {
+            var reader = new EnumerableTextReader(Console.In);
+            reader.ForEach(Console.WriteLine);
+        }
+    }
+
+    public static class EnumerableExtentions {
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action) {
+            foreach (T x in source) {
+                action(x);
+            }
+        }
+    }
+}
