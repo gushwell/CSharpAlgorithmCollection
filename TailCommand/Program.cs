51df3b706469b55121a22a27b27ac94831fd58be
gushwell <gushwell@gmail.com>
2018-11-27 17:33:19 +0900

    RingBufferクラスを作成し、簡易Tailコマンドを書く

---
+++ TailCommand/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace TailCommand {
+    class Program {
+        static void Main(string[] args) {
+            int count = 10;
+            string filepath = null;
+            if (args.Length > 0) {
+                if (int.TryParse(args[0], out count) == false) {
+                    count = 10;
+                    filepath = args[0];
+                } else if (args.Length == 2)
+                    filepath = args[1];
+            }
+            DoTail(count, filepath);
+        }
+
+        private static void DoTail(int linecount, string filename) {
+            using (var tr = string.IsNullOrEmpty(filename)
+                                ? Console.In : new StreamReader(filename)) {
+                RingBuffer<string> rb = new RingBuffer<string>(linecount);
+                string line;
+                while ((line = tr.ReadLine()) != null) {
+                    rb.Add(line);
+                }
+                foreach (var s in rb)
+                    Console.WriteLine(s);
+            }
+        }
+
+    }
+}
---
+++ TailCommand/RingBuffer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TailCommand {
+    public class RingBuffer<T> : IEnumerable<T> {
+        private int _size;
+        private T[] _buffer;
+        private int _writeIndex = -1;    // 書き終わった位置
+        private bool _isFull = false;
+
+        public RingBuffer(int size) {
+            _size = size;
+            _buffer = new T[size];
+            _writeIndex = -1;
+        }
+
+        private int NextIndex(int ix) {
+            return ++ix % _size;
+        }
+
+        private int GetStartIndex() {
+            return _isFull ? NextIndex(_writeIndex) : 0;
+        }
+
+
+        public void Add(T value) {
+            _writeIndex = NextIndex(_writeIndex);
+            if (_writeIndex == _size - 1)
+                _isFull = true;
+            _buffer[_writeIndex] = value;
+        }
+
+        public bool Exists {
+            get { return Count > 0; }
+        }
+
+        public int Count {
+            get { return _isFull ? _size : _writeIndex + 1; }
+        }
+
+        public void Clear() {
+            _writeIndex = -1;
+            _isFull = false;
+        }
+
+        public IEnumerator<T> GetEnumerator() {
+            var index = GetStartIndex();
+            for (int i = 0; i < Count; i++) {
+                yield return _buffer[index];
+                index = NextIndex(index);
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator() {
+            return this.GetEnumerator();
+        }
+    }
+}
